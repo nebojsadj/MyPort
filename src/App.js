9e2ebf018bf8424a0c7306282daae618d9716d47
Nebojsadj <djordjevicn87@gmail.com>
2021-02-15 14:57:53 +0100

    adding ScrollToTop.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Message from "./components/Message";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div>
       <Header />
+      <ScrollToTop />
       <Switch>
         <Route path="/" exact>
           <About />
