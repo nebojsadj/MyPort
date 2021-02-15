92d7e0cbd5f8ffacb1ac590bdc71365c6030d4e4
Nebojsadj <djordjevicn87@gmail.com>
2021-02-15 14:24:59 +0100

    removed collapseOnSelect from navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,36 +4,28 @@ import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <Navbar
-      collapseOnSelect
-      expand="md"
-      bg="transparent"
-      className="d-flex justify-content-end mt-2"
-    >
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mx-auto">
-          <NavLink
-            to="/"
-            exact
-            className="justify-content-center p-2 text-light nav"
-          >
-            About Me
-          </NavLink>
-          <NavLink
-            to="/projects"
-            className="justify-content-center p-2 text-light nav"
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className="justify-content-center p-2 text-light nav"
-          >
-            Contact
-          </NavLink>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar bg="transparent">
+      <Nav className="mx-auto">
+        <NavLink
+          to="/"
+          exact
+          className="justify-content-center p-2 text-light nav"
+        >
+          About Me
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="justify-content-center p-2 text-light nav"
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className="justify-content-center p-2 text-light nav"
+        >
+          Contact
+        </NavLink>
+      </Nav>
     </Navbar>
   );
 }
